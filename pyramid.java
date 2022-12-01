5901d9d5aa08ce59e48a805f63fece6f2c377b55
Jin Ustad (Gul Mohammad) <96934693+JinUstad@users.noreply.github.com>
2022-12-01 07:38:10 +0530

    Add files via upload

---
+++ pyramid.java
@@ -0,0 +1,14 @@
+public class pyramid {
+    public static void main(String[] args) {
+        System.out.println("Pyramid pattern ");
+        for(int i = 1 ; i<=6;i++){
+            for(int j = 1 ; j<=6-i ; j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+}
